1dd97937f608bf4d91951e1fc7c52bf6e3811afd
Ömer Faruk Demirel <omerfdmrl@gmail.com>
2025-10-24 21:30:03 +0300

    [LINT] fix code style

--- app/Support/FileNamer.php
+++ app/Support/FileNamer.php
@@ -5,7 +5,8 @@ namespace App\Support;
 use Illuminate\Support\Str;
 use Spatie\MediaLibrary\Support\FileNamer\DefaultFileNamer;
 
-class FileNamer extends DefaultFileNamer {
+class FileNamer extends DefaultFileNamer
+{
     public function originalFileName(string $fileName): string
     {
         return Str::uuid()->toString();
