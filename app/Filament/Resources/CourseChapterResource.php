e43c03097f58a82c6e6f47da470dfd6bb58fb2e4
Ömer Faruk Demirel <omerfdmrl@gmail.com>
2025-11-23 12:18:32 +0300

    feat(panel): Add filters and actions for course resource

--- app/Filament/Resources/CourseChapterResource.php
+++ app/Filament/Resources/CourseChapterResource.php
@@ -54,7 +54,9 @@ class CourseChapterResource extends Resource
 
             ])
             ->actions([
+                Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
